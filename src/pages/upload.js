671696f70cc7b8a3700e0ced302545ef3a9d4d31
mattdvhope <mattagape@me.com>
2020-05-09 10:10:52 +0700

    Set up uploading to AWS S3

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 import get from 'lodash/get'
 import AWS from 'aws-sdk';
 import Layout from '../components/layout'
-
+import photo from '../../static/asianTeam.jpg'
 
 export default class Upload extends React.Component {
 	constructor() {
@@ -15,12 +15,15 @@ export default class Upload extends React.Component {
       s3data: undefined,
       albums: undefined,
       newAlbumName: "",
+      albumChosenName: "",
       newAlbumExists: false,
       bucketName: `cep-class-notes`,
+      photoURL: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addPhoto = this.addPhoto.bind(this);
   }
 
   componentDidMount() {
@@ -32,13 +35,13 @@ export default class Upload extends React.Component {
 		  })
 		});
 
+
 		const s3 = new AWS.S3({
 		  apiVersion: "2006-03-01",
 		  params: { Bucket: bucketName }
 		});
 
 		const _this = this;
-
 	  s3.listObjects({ Delimiter: "/" }, function(err, data) {
 	    if (err) {
 	      return alert("There was an error listing your albums: " + err.message);
@@ -81,26 +84,62 @@ export default class Upload extends React.Component {
 	        return alert("There was an error creating your album: " + err.message);
 	      }
 	      alert("Successfully created album.");
-	      _this.viewNewAlbum(albumName);
+	      _this.viewAlbums(albumName)
 	    });
 	  });
 	}
 
-	viewNewAlbum(albumName) {
+	async viewAlbums(albumName) {
 		const s3 = this.state.s3;
-		const albumPhotosKey = encodeURIComponent(albumName) + "//";
-		const listObjects = s3.listObjects({ Prefix: albumPhotosKey }, (err, data) => {
-			if (err) {
+		const _this = this;
+		const objs = await s3.listObjects({}, function(err, data) {
+	    if (err) {
 	      return alert("There was an error viewing your album: " + err.message);
+	    }	
+	    const contents = data.Contents;
+      const last_element = contents[contents.length - 1];
+      _this.state.s3data.CommonPrefixes.push({Prefix: albumName + "/"})
+      console.log(last_element)
+      console.log(_this.state.s3data.CommonPrefixes)
+      _this.setState(_this.state);
+      document.getElementById("albInput").value = "";
+      console.log(document.getElementById("albInput").value)
+		})
+	}
+
+	addPhoto() {
+		const files = document.getElementById("photoupload").files;
+		if (!files.length) {
+	    return alert("Please choose a file to upload first.");
+	  }
+		const file = files[0];
+	  const fileName = file.name;
+		const albumName = this.state.albumChosenName
+		const albumPhotosKey = encodeURIComponent(albumName) + "/";
+		const photoKey = albumPhotosKey + fileName;
+
+		const upload = new AWS.S3.ManagedUpload({
+	    params: {
+	      Bucket: this.state.bucketName,
+	      Key: photoKey,
+	      Body: file,
+	      ACL: "public-read"
 	    }
-	    return data;
-		});
+	  });
 
-    const href = listObjects.response.request.httpRequest.endpoint.href;
-    const bucketUrl = href + this.state.bucketName + "/";
-
-		console.log("bucketUrl: ", bucketUrl);
-
+	  const promise = upload.promise();
+		const _this = this;
+	  promise.then(
+	    function(response) {
+	    	console.log(response)
+				_this.setState({ photoURL: response.Location });
+	      // alert("Successfully uploaded photo.");
+	      // viewAlbum(albumName);
+	    },
+	    function(err) {
+	      return alert("There was an error uploading your photo: ", err.message);
+	    }
+	  );
 	}
 
   render() {
@@ -113,7 +152,14 @@ export default class Upload extends React.Component {
 	      const albums = data.CommonPrefixes.map((commonPrefix) => {
 	        const prefix = commonPrefix.Prefix;
 	        const albumName = decodeURIComponent(prefix.replace("/", ""));
-					return (<li key={albumName}>{albumName}</li>)
+					return (
+						<li
+							key={albumName}
+							onClick={e => this.setState({ albumChosenName: albumName })}
+						>
+							{albumName}
+						</li>
+					)
 	      });
 				return (
 			    <ul>{albums}</ul>
@@ -131,13 +177,20 @@ export default class Upload extends React.Component {
 					<form onSubmit={this.handleSubmit}>
 		        <label>
 		          New Album:
-		          <input value={this.state.newAlbumName} onChange={this.handleChange} />
+		          <input id="albInput" value={this.state.newAlbumName} onChange={this.handleChange} />
 		        </label>
 		        <input type="submit" value="Submit" />
 		      </form>
+					
+					<div>Before choosing photo, click an Album above </div>
+					<h3>{this.state.albumChosenName}</h3>
 
-					{}
+					<input id="photoupload" type="file" />
+					<br/>
+					<button onClick={this.addPhoto}>Upload file you've chosen</button>
 
+					{  }
+					<img src={this.state.photoURL} alt=""/>
 
 			  </Layout>
 			)
